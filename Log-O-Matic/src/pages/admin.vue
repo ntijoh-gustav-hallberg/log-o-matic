acb0aab2e0d6262c4b4c8ff8ddded3dd6c933d7a
Gustav0113 <gustav.hallberg05@gmail.com>
2024-11-15 13:40:10 +0100

    added admin page, teacher and student cards

---
+++ Log-O-Matic/src/pages/admin.vue
@@ -0,0 +1,232 @@
+<script>
+import {useTeacherStore, useStudentStore} from "../stores/app.ts"
+
+const teacherStore = useTeacherStore();
+const studentStore = useStudentStore();
+
+// For teacher table
+    export default {
+        data () {
+            return {
+                teacherHeaders: [
+                    { title: 'email', align: 'start', key: 'email' },
+                    { title: 'name', align: 'start', key: 'name' },
+                    { title: '', align: 'end', key: 'password' },
+
+                ],
+                teachers: teacherStore.getAll,
+
+                studentHeaders: [
+                    { title: 'email', align: 'start', key: 'email' },
+                    { title: 'name', align: 'start', key: 'name' },
+                    { title: 'teacher', align: 'start', key: 'teacher' },
+                    { title: '', align: 'end', key: 'password' },
+
+                ],
+                students: studentStore.getAll,
+            }
+        },
+  
+        computed: {
+            virtualTeachers () {
+                return [...Array(this.teachers.length).keys()].map(i => {
+                    const teacher = { ...this.teachers[i % this.teachers.length] }
+                    return teacher
+                })
+            },
+
+            virtualStudents () {
+                return [...Array(this.students.length).keys()].map(i => {
+                    const student = { ...this.students[i % this.students.length] }
+                    return student
+                })
+            },
+        },
+
+        setup() {
+            // Get the list of teacher names
+            const teacherNames = computed(() => teacherStore.teachers.map(teacher => teacher.name));
+
+            return {
+                teacherNames,
+            };
+        },
+    }
+</script>
+
+<template>
+    <v-card title="Teachers" class="ma-5">
+        <v-data-table-virtual
+            :headers="teacherHeaders"
+            :items="virtualTeachers"
+            item-value="name"
+        >
+        <template #item.password="{ item }">
+            <div class="d-flex align-center">
+                <!-- Input Field -->
+                <v-text-field
+                label="Password"
+                variant="underlined"
+                hide-details
+                v-model="item.password"
+                type="password"
+                dense
+                ></v-text-field>
+
+                <!-- Button -->
+                <v-btn color="blue" rounded="2">
+                Reset
+                </v-btn>
+            </div>
+        </template>
+        </v-data-table-virtual>
+
+        <v-card-subtitle class="my-4">Add New Teacher</v-card-subtitle>
+        <v-form v-model="valid">
+            <v-container>
+            <v-row>
+                <v-col
+                cols="12"
+                md="4"
+                >
+                <v-text-field
+                    v-model="email"
+                    label="Email"
+                    variant="underlined"
+                    required
+                ></v-text-field>
+                </v-col>
+
+                <v-col
+                cols="12"
+                md="4"
+                >
+                <v-text-field
+                    v-model="name"
+                    label="Name"
+                    variant="underlined"
+                    required
+                ></v-text-field>
+                </v-col>
+
+                <v-col
+                cols="12"
+                md="4"
+                >
+                <v-text-field
+                    v-model="password"
+                    :rules="passwordRules"
+                    label="Password"
+                    variant="underlined"
+                    required
+                    type="password"
+                ></v-text-field>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-spacer></v-spacer>
+                <v-btn color="green" rounded="2" class="mb-2">
+                Create
+                </v-btn>
+            </v-row>
+            </v-container>
+        </v-form>
+    </v-card>
+
+    <v-card title="Students" class="ma-5">
+        <v-data-table-virtual
+            :headers="studentHeaders"
+            :items="virtualStudents"
+            item-value="name"
+        >
+        <template #item.teacher="{ item }">
+                <!-- Input Field -->
+                <v-select
+                label="Teacher"
+                :items="teacherNames"
+                variant="underlined"
+                ></v-select>
+        </template>
+        <template #item.password="{ item }">
+            <div class="d-flex align-center">
+                <!-- Input Field -->
+                <v-text-field
+                label="Password"
+                variant="underlined"
+                hide-details
+                v-model="item.password"
+                type="password"
+                dense
+                ></v-text-field>
+
+                <!-- Button -->
+                <v-btn color="blue" rounded="2">
+                Reset
+                </v-btn>
+            </div>
+        </template>
+        </v-data-table-virtual>
+
+        <v-card-subtitle class="my-4">Add New Student</v-card-subtitle>
+        <v-form v-model="valid">
+            <v-container>
+            <v-row>
+                <v-col
+                cols="12"
+                md="4"
+                >
+                <v-text-field
+                    v-model="email"
+                    label="Email"
+                    variant="underlined"
+                    required
+                ></v-text-field>
+                </v-col>
+
+                <v-col
+                cols="12"
+                md="4"
+                >
+                <v-text-field
+                    v-model="name"
+                    label="Name"
+                    variant="underlined"
+                    required
+                ></v-text-field>
+                </v-col>
+
+                <v-col
+                cols="12"
+                md="4"
+                >
+                <v-select
+                label="Teacher"
+                :items="teacherNames"
+                variant="underlined"
+                ></v-select>
+                </v-col>
+
+                <v-col
+                cols="12"
+                md="4"
+                >
+                <v-text-field
+                    v-model="password"
+                    :rules="passwordRules"
+                    label="Password"
+                    variant="underlined"
+                    required
+                    type="password"
+                ></v-text-field>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-spacer></v-spacer>
+                <v-btn color="green" rounded="2" class="mb-2">
+                Create
+                </v-btn>
+            </v-row>
+            </v-container>
+        </v-form>
+    </v-card>
+</template>
