6bdc504dca101acd5e1aa5febd5c33b2b5b26b91
PhilipBranting05 <philip@agbilverkstad.se>
2024-11-15 12:00:10 +0100

    basic navbar

--- Log-O-Matic/src/layouts/default.vue
+++ Log-O-Matic/src/layouts/default.vue
@@ -1,13 +1,41 @@
 <template>
   <v-app>
+    <v-app-bar :elevation="12" rounded>
+
+    <v-app-bar-title @click="StartMenu()">Log-O-Matic</v-app-bar-title>
+
+    <template v-slot:append>
+      <v-btn v-if="!token" @click="LoginMenu()">
+        <v-icon  icon="mdi-lock"/>
+        logga in
+      </v-btn>
+      <v-btn v-if="token">
+        <v-icon  icon="mdi-lock-open"/>
+        logga ut
+      </v-btn>
+      </template>
+    </v-app-bar>
+<br>
     <v-main>
       <router-view />
     </v-main>
+
 
     <AppFooter />
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+import router from '@/router';
+
+  const token = "";
+
+
+  function StartMenu(){
+    router.push('/')
+  }
+
+  function LoginMenu(){
+    router.push('/loginPage')
+  }
 </script>
