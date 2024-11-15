83605232e3255f4250570e787dbba8aef476e7c6
PhilipBranting05 <philip@agbilverkstad.se>
2024-11-15 16:26:14 +0100

    getting week date and sending with query

--- Log-O-Matic/src/layouts/default.vue
+++ Log-O-Matic/src/layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <v-btn @click="goToLogs">logs</v-btn>
     <v-main>
       <router-view />
     </v-main>
@@ -9,5 +10,21 @@
 </template>
 
 <script lang="ts" setup>
-  //
+import router from '@/router';
+import { getISOWeek } from 'date-fns';
+
+const today = new Date();
+const weekNumber = getISOWeek(today);
+
+console.log(`Today is in week: ${weekNumber}`);
+
+const goToLogs = () => {
+  router.push({
+    path: '/logs',
+    query: {
+      week: weekNumber,
+    },
+  });
+};
+
 </script>
---
+++ Log-O-Matic/src/pages/logs.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>{{ week }}</p>
+    </template>
+    
+    <script lang="ts" setup>
+    import { useRoute } from 'vue-router';
+    const route = useRoute();
+    
+    const week = route.query.week;
+    console.log(week)
+    </script>
